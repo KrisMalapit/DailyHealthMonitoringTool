1719c03588e7a80d36669ebcdfcbf253ddb1957c
kcmalapit <kcmalapit@WSD2012>
2022-01-19 09:25:32 +0800

    01192021

--- ScreeningTool/Models/ScreenLogs.cs
+++ ScreeningTool/Models/ScreenLogs.cs
@@ -42,6 +42,7 @@ namespace ScreeningTool.Models
         public int Q19 { get; set; }
         public int Q20 { get; set; }
         public int Q21 { get; set; }
+        public int Q22 { get; set; }
 
         public string RemarksQ1 { get; set; }
         public string RemarksQ2 { get; set; }
